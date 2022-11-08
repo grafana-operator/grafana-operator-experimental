ae56a035bbcf545cd52376f713116a27e88880ee
Peter Braun <pbraun@redhat.com>
2022-11-08 16:07:24 +0100

    sync delay as a constant

--- controllers/dashboard_controller.go
+++ controllers/dashboard_controller.go
@@ -37,6 +37,10 @@ import (
 	stderr "errors"
 )
 
+const (
+	initialSyncDelay = "10s"
+)
+
 // GrafanaDashboardReconciler reconciles a GrafanaDashboard object
 type GrafanaDashboardReconciler struct {
 	Client    client.Client
@@ -332,7 +336,7 @@ func (r *GrafanaDashboardReconciler) SetupWithManager(mgr ctrl.Manager, stop cha
 		Complete(r)
 
 	if err == nil {
-		d, err := time.ParseDuration("10s")
+		d, err := time.ParseDuration(initialSyncDelay)
 		if err != nil {
 			return err
 		}
